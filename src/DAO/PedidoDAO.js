71d8383195dc062ad02f9b8861e68553656c0725
jeanetchichury <jean.etchichury@hotmail.com>
2021-11-27 16:00:09 -0300

    Correção erros e bugs

--- src/DAO/PedidoDAO.js
+++ src/DAO/PedidoDAO.js
@@ -3,113 +3,113 @@ class PedidoDAO{
         this.bd = bd
     }
 
-    // pegaTodosUsuarios(){
-    //     return new Promise((resolve, reject)=>{
-    //         this.bd.all('SELECT * FROM PEDIDO', (error, rows)=>{
-    //             if(error){
-    //                 reject({
-    //                     "mensagem" : error.message,
-    //                     "error" : true
-    //                 }) 
-    //             } else{
-    //                 resolve({
-    //                     "usuarios" : rows,
-    //                     "count": rows.length,
-    //                     "error" : false
-    //                 })
-    //             }
-    //         })
-    //     })  
-    // }
+    pegaTodosPedido(){
+        return new Promise((resolve, reject)=>{
+            this.bd.all('SELECT * FROM PEDIDO', (error, rows)=>{
+                if(error){
+                    reject({
+                        "mensagem" : error.message,
+                        "error" : true
+                    }) 
+                } else{
+                    resolve({
+                        "pedido" : rows,
+                        "count": rows.length,
+                        "error" : false
+                    })
+                }
+            })
+        })  
+    }
 
-    // inserePedido(novoPedido){
-    //     return new Promise((resolve, reject)=>{
-    //         this.bd.run(`INSERT INTO PEDIDO (CLIENTE, STATUS_PG, ITEM1, QNTD_ITEM1, ITEM2, QNTD_ITEM2, ITEM3, QNTD_ITEM3) VALUES (?,?,?,?,?,?,?,?)`,
-    //         [novoPedido.cliente, novoPedido.status_pg, novoPedido.item1, novoPedido.qntd_item1, novoPedido.item2, novoPedido.qntd_item2, novoPedido.item3, novoPedido.qntd_item3], 
-    //         (error)=>{
-    //             if(error){
-    //                 reject({
-    //                     "mensagem" : error.message,
-    //                     "erro" : true 
-    //                 })
-    //             } else {
-    //                 resolve({
-    //                     "requisicao" : novoUsuario,
-    //                     "erro" : false 
-    //                 })
-    //             }
-    //         })
-    //     })
-    // }
+    inserePedido(novoPedido){
+        return new Promise((resolve, reject)=>{
+            this.bd.run(`INSERT INTO PEDIDO (CLIENTE, STATUS_PG, ITEM1, QNTD_ITEM1, ITEM2, QNTD_ITEM2, ITEM3, QNTD_ITEM3) VALUES (?,?,?,?,?,?,?,?)`,
+            [novoPedido.cliente, novoPedido.status_pg, novoPedido.item1, novoPedido.qntd_item1, novoPedido.item2, novoPedido.qntd_item2, novoPedido.item3, novoPedido.qntd_item3], 
+            (error)=>{
+                if(error){
+                    reject({
+                        "mensagem" : error.message,
+                        "erro" : true 
+                    })
+                } else {
+                    resolve({
+                        "requisicao" : novoPedido,
+                        "erro" : false 
+                    })
+                }
+            })
+        })
+    }
 
-    // pegaPedidoPorCliente(cliente){
-    //     const SELECT_BY_CLIENTE = `
-    //     SELECT * FROM CLIENTE
-    //     WHERE CLIENTE = ?`
-    //     return new Promise((resolve, reject)=>{
-    //         this.bd.all(SELECT_BY_CLIENTE, cliente, (error, rows)=>{
-    //             if(error){
-    //                 reject({
-    //                     "mensagem" : error.message,
-    //                     "erro" : true 
-    //                 })
-    //             } else {
-    //                 resolve({
-    //                     "requisicao" : rows,
-    //                     "erro" : false 
-    //                 })
-    //             }
-    //         })
-    //     })
-    // }
+    pegaPedidoPorCliente(cliente){
+        const SELECT_BY_CLIENTE = `
+        SELECT * FROM CLIENTE
+        WHERE CLIENTE = ?`
+        return new Promise((resolve, reject)=>{
+            this.bd.all(SELECT_BY_CLIENTE, cliente, (error, rows)=>{
+                if(error){
+                    reject({
+                        "mensagem" : error.message,
+                        "erro" : true 
+                    })
+                } else {
+                    resolve({
+                        "requisicao" : rows,
+                        "erro" : false 
+                    })
+                }
+            })
+        })
+    }
     
-    // pegaPedidoPorId(id){
-    //     const SELECT_BY_ID = `
-    //     SELECT * FROM PEDIDO
-    //     WHERE ID = ?`
-    //     return new Promise((resolve, reject)=>{
-    //         this.bd.all(SELECT_BY_ID, id, (error, rows)=>{
-    //             if(error) {
-    //                 reject({
-    //                     "mensagem" : error.message,
-    //                     "erro" : true 
-    //                 })
-    //             } else {
-    //                 resolve({
-    //                     "requisicao" : rows,
-    //                     "erro" : false 
-    //                 })
-    //             }
-    //         })
-    //     })
-    // }
+    pegaPedidoPorId(id){
+        const SELECT_BY_ID = `
+        SELECT * FROM PEDIDO
+        WHERE ID = ?`
+        return new Promise((resolve, reject)=>{
+            this.bd.all(SELECT_BY_ID, id, (error, rows)=>{
+                if(error) {
+                    reject({
+                        "mensagem" : error.message,
+                        "erro" : true 
+                    })
+                } else {
+                    resolve({
+                        "requisicao" : rows,
+                        "erro" : false 
+                    })
+                }
+            })
+        })
+    }
 
-    // async deletaPedido(id){
-    //     try {
-    //         const pedido = await this.pegaPedidoPorId(id)
-    //         if(pedido.requisicao.length){
-    //             const DELETE = `
-    //             DELETE FROM PEDIDO
-    //             WHERE ID = ?`
-    //             return new Promise((resolve, reject)=>{
-    //                 this.bd.run(DELETE, id, (error)=>{
-    //                     if(error){
-    //                         reject(error.message)
-    //                     } else {
-    //                         resolve({
-    //                             "mensagem" : `Usuário de id ${id} deletado`,
-    //                             "erro" : false 
-    //                         })
-    //                     }
-    //                 })
-    //             })
-    //         } else {
-    //             throw new Error(`Usuário de id ${id} não existe`)
-    //         }
-    //     } catch (error) {
-    //         throw new Error(error.message)
-    //     }   
-    // }
+    async deletaPedido(id){
+        try {
+            const pedido = await this.pegaPedidoPorId(id)
+            if(pedido.requisicao.length){
+                const DELETE = `
+                DELETE FROM PEDIDO
+                WHERE ID = ?`
+                return new Promise((resolve, reject)=>{
+                    this.bd.run(DELETE, id, (error)=>{
+                        if(error){
+                            reject(error.message)
+                        } else {
+                            resolve({
+                                "mensagem" : `Usuário de id ${id} deletado`,
+                                "erro" : false 
+                            })
+                        }
+                    })
+                })
+            } else {
+                throw new Error(`Usuário de id ${id} não existe`)
+            }
+        } catch (error) {
+            throw new Error(error.message)
+        }   
+    }
 
     async atualizaPedido(id, novoPedido){
         try {
@@ -126,7 +126,7 @@ class PedidoDAO{
                     } else {
                         resolve({
                             "mensagem" : `Pedido de id ${id} atualizado`,
-                            "usuario": novoPedido,
+                            "pedido": novoPedido,
                             "erro" : false 
                         })
                     }
